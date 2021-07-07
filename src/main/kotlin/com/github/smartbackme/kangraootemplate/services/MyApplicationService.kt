c7412796f0f2f264ff67beebb6040538e2530ab5
GitHub Action <action@github.com>
2021-07-07 03:42:56 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/smartbackme/kangraootemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.smartbackme.kangraootemplate.services
+
+import com.github.smartbackme.kangraootemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/smartbackme/kangraootemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/smartbackme/kangraootemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.smartbackme.kangraootemplate.services
 
+import com.github.smartbackme.kangraootemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
